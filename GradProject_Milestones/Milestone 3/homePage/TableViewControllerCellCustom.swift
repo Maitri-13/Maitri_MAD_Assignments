6b443147100a40cde5f9af730087b0c8e17e60ab
Maitri Chattopadhyay <mach5953@colorado.edu>
2017-12-11 16:49:34 -0700

    Milestone 3 Project Folder

---
+++ GradProject_Milestones/Milestone 3/homePage/TableViewControllerCellCustom.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewControllerCellCustom.swift
+//  homePage
+//
+//  Created by Maitri Chattopadhyay on 10/26/17.
+//  Copyright © 2017 Maitri Chattopadhyay. All rights reserved.
+//
+
+import UIKit
+
+class TableViewControllerCellCustom: UITableViewCell {
+
+    @IBOutlet weak var taskDesc: UILabel!
+    @IBOutlet weak var taskDate: UILabel!
+    @IBOutlet weak var taskName: UILabel!
+
+}
