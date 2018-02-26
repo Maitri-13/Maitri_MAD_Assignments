8e5cae5409f1b92d707f25081b32642ae077d3b8
Maitri Chattopadhyay <mach5953@colorado.edu>
2018-02-25 21:09:01 -0700

    Lab 4

---
+++ Advanced MAD_2018/Lab4/Lab4_1.0/Lab4_1.0/petetion.swift
@@ -0,0 +1,18 @@
+//
+//  petetion.swift
+//  Lab4_1.0
+//
+//  Created by Maitri Chattopadhyay on 2/25/18.
+//  Copyright © 2018 Maitri Chattopadhyay. All rights reserved.
+//
+
+import Foundation
+
+struct petetion: Decodable{
+    
+    let title: String
+    let sigCount : Int
+    let url: String
+    
+}
+
